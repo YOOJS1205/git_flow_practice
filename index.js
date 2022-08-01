699419f2bb67a4c21075c898f1322a640b6fa02c
jisu-kim <kss2460@naver.com>
2022-08-01 15:21:04 +0900

    Docs: 파일 삭제

---
+++ index.js
@@ -0,0 +1,12 @@
+var heights = [1, 4, 2, 5, 3];
+
+heights.forEach((e, i) => {
+  console.log(e);
+});
+
+for (let i = 0; i < heights.length; i++) {}
+
+function solution(heights) {
+  var answer = 0;
+  return answer;
+}
---
+++ jisu.js
@@ -0,0 +1,2 @@
+feature2
+const 지수 = () => console.log('지수')
